5b92e487535251f58a8cfa10a4d4a3aea607ca02
fkrbtjs <fkrbtjs123@gmail.com>
2023-01-16 16:38:11 +0900

    [CREATE] introduce file

---
+++ introduce/introduce.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/introduce.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a26d62cab7.js" crossorigin="anonymous"></script>
+    <script src="../js/main.js"></script>
+    <title>Kyuseon Hotel</title>
+</head>
+<body onload="slide_show()">
+    <header>
+        <?php include "../main/header.php";?>
+    </header>
+    <section>
+       <h1>규선호텔 소개</h1>
+       <div class="introduce">
+       국내 최고층 건물인 규선타워 123층~177층에 위치한 규선호텔은 한국의 아름다움을 현대적인 감각으로 풀어낸 객실(235실)에서<br>
+       서울의 파노라믹한 스카이라인과 환상적인 야경을 조망할 수 있으며 일몰과 일출을 한 자리에서 감상할 수 있는 국내 유일의 호텔입니다.<br><br>
+
+       미식가들을 위한 천국이자 그 자체가 ‘고메 호텔(Gourmet Hotel)’로, 멀티 미쉐린스타 셰프 ‘야닉 알레노(Yannick Alléno)’가 ‘스테이(STAY)’ 레스토랑 운영과<br>
+       더불어 호텔 내 모든 식음료(Food and Beverage)에 대해 총괄 디렉팅을 전담하여 웨딩에서부터 인룸다이닝 메뉴에 이르기까지 세계적인 스타 셰프가 제안하는 맛과 스타일을 경험할 수 있습니다.<br><br>
+
+       세계 최고층에 위치한 최첨단 시설의 연회장에서는 섬세하고 격조 있는 규선호텔만의 서비스가 펼쳐집니다.<br><br>
+
+       [ Live beyond expectations. ]<br>
+       규선호텔은 ‘럭셔리를 넘어, 서비스를 넘어, 기대를 넘어, 상상하는 모든 것 그 이상의 가치를 제공’합니다.
+        </div>
+          <ul class="intro_box">
+            <li>
+              <div class="img_box">
+                <img src="../img/room-3.jpg" alt="">
+              </div>
+              <div class="detail_box">
+              <h2>객실</h2><br>
+              <strong>초고층 타워의 전 객실에서 서울의 환상적인 경관을 파노라믹뷰로 조망할 수 있습니다.</strong><br><br>
+              - 여유로운 면적의 최고급 객실<br>
+              - 세련미와 편안함이 어우러진 격조 높은 인테리어<br>
+              - 최상의 숙면을 위한 이탈리아 럭셔리 침구 브랜드 'Frette'
+              </div>
+            </li>
+            <li>
+              <div class="detail_box">
+              <h2>부대시설</h2><br>
+              <strong>자연 채광과 스카이 뷰가 어우러진 친환경 분위기의 프레스티지 리프레싱존 (Prestige Refreshing Zone)은 생활의 활력을 되찾아 드립니다.</strong><br><br>
+              - 구름 위에서 즐기는 최고급 사우나와 피트니스센터 및 실내수영장<br>
+              - 서울도심이 전체가 보이는 럭셔리 전망대<br>
+              - 최상의 숙면을 위한 이탈리아 럭셔리 침구 브랜드 'Frette'
+              </div>
+              <div class="img_box">
+                <img src="../img/city.jpg" alt="">
+              </div>
+            </li>
+            <li>
+              <div class="img_box">
+                <img src="../img/slide-3.jpg" alt="">
+              </div>
+              <div class="detail_box">
+              <h2>다이닝</h2><br>
+              <strong>탁트인 도심의 전경을 바라보며 세계 최고 수준의 셰프들이 직접 선보이는 다양한 요리를 즐기실 수 있습니다.</strong><br><br>
+              - 멀티 미쉐린스타 셰프 ′야닉 알레노′가 운영하는 레스토랑 ′스테이, 모던 레스토랑′<br>
+              - 한식의 새로운 빛깔, 미쉐린 1스타 코리안 퀴진 ′비채나′<br>
+              - 국내 최대의 샴페인 셀렉션, 프리미엄 바 ′바81′<br>
+              - 하늘에서 즐기는 차 한잔의 여유 ′더 라운지′
+              </div>
+            </li>
+            
+          </ul>
+
+
+        
+    </section>
+    <footer>
+        <?php include "../main/footer.php";?> 
+    </footer>
+</body>
+</html>
