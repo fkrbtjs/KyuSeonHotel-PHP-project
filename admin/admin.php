c63bb38f84f1473eb2087cc2dcf3182d275d5762
KyuSeon Ra <115532120+fkrbtjs@users.noreply.github.com>
2023-01-29 22:47:52 +0900

    [UPDATE] admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -2,7 +2,7 @@
 <html>
 <head> 
 <meta charset="utf-8">
-<title>PHP 프로그래밍 입문</title>
+<title>admin</title>
 <link rel="stylesheet" type="text/css" href="../css/main.css">
 <link rel="stylesheet" type="text/css" href="../css/admin.css">
 <script src="https://kit.fontawesome.com/a26d62cab7.js" crossorigin="anonymous"></script>
