a962cff35a928c1ecbcccc2bbd12c574cf5debb9
KyuSeon Ra <115532120+fkrbtjs@users.noreply.github.com>
2023-01-29 22:49:48 +0900

    [UPDATE] admin_dui.php

--- admin/admin_member_dui.php
+++ admin/admin_member_dui.php
@@ -1,6 +1,5 @@
 <?php
    session_start();
-
    $userlevel = isset($_SESSION['userlevel']) ? $userlevel = $_SESSION['userlevel'] : ""; 
 
    if ( $userlevel != 'admin' ) {
@@ -14,21 +13,16 @@
    }
 
    include('../db/main_db.php');
-
    $mode = $_GET["mode"];
 
    switch ($mode) {
-      case "update" : {
-        
+      case "update" : {     
          $num   = $_POST["num"];
          $level = $_POST["level"];
          $point = $_POST["point"];
          $name = $_POST["name"];
-
          $sql = "update members set level= '$level', point= $point where num = $num";
          mysqli_query($con, $sql);
-         // session_start();
-         // $_SESSION["userpoint"] = $point;
          break;
       }
 
@@ -43,7 +37,6 @@
          if (isset($_POST["item"])) {
             $num_item = count($_POST["item"]); 
          }   else {
-
             echo("
             <script>
             alert('삭제할 게시글을 선택해주세요!');
@@ -51,21 +44,18 @@
             </script>
             ");
          }
-            for($i = 0; $i < $num_item; $i++){
-               
-               $num = $_POST["item"][$i];
-               
+         
+            for($i = 0; $i < $num_item; $i++){               
+               $num = $_POST["item"][$i];              
                $sql = "select * from image_board where num = $num";
                $result = mysqli_query($con, $sql);
-               $row = mysqli_fetch_array($result);
-               
+               $row = mysqli_fetch_array($result);               
                $copied_name = $row["file_copied"];
                
                if ($copied_name) {
                   $file_path = "./data/".$copied_name;
                   unlink($file_path);
-               }
-               
+               }              
                $sql = "delete from image_board where num = $num";
                mysqli_query($con, $sql);
             }   
